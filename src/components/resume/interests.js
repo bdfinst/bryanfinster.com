b2cfaa51fac5f6471bf9a815d505282b9884681f
Bryan Finster <bryan.finster@gmail.com>
2021-03-19 08:02:17 -0500

    Fix linting issues (#10)
    * Change node versions
    
    * Resolve linting
    
    * Add husky back

--- src/components/resume/interests.js
+++ src/components/resume/interests.js
@@ -7,51 +7,49 @@ const Interests = (props) => {
   const renderData = () => {
     const { dataArr } = props
 
-    return dataArr.map((data) => {
-      return (
-        <Container key={data.name} className="tagListContainer">
-          <Header as="h3">
-            <Icon name="hand spock" color="teal" />
-            {data.name}
-          </Header>
-          <p>
-            {data.keywords && data.keywords.length > 0 ? (
-              data.keywords.map((keyword) => {
-                const colors = [
-                  'red',
-                  'orange',
-                  'yellow',
-                  'olive',
-                  'green',
-                  'teal',
-                  'blue',
-                  'violet',
-                  'purple',
-                  'pink',
-                  'brown',
-                  'grey',
-                  'black',
-                ]
-                const color = colors[Math.floor(Math.random() * colors.length)]
-                return (
-                  <Label key={keyword} as="a" className="tagItem" color={color}>
-                    <Icon name="tag" />
-                    {keyword}
-                  </Label>
-                )
-              })
-            ) : (
-              <div />
-            )}
-          </p>
-        </Container>
-      )
-    })
+    return dataArr.map((data) => (
+      <Container key={data.name} className="tagListContainer">
+        <Header as="h3">
+          <Icon name="hand spock" color="teal" />
+          {data.name}
+        </Header>
+        <p>
+          {data.keywords && data.keywords.length > 0 ? (
+            data.keywords.map((keyword) => {
+              const colors = [
+                'red',
+                'orange',
+                'yellow',
+                'olive',
+                'green',
+                'teal',
+                'blue',
+                'violet',
+                'purple',
+                'pink',
+                'brown',
+                'grey',
+                'black',
+              ]
+              const color = colors[Math.floor(Math.random() * colors.length)]
+              return (
+                <Label key={keyword} as="a" className="tagItem" color={color}>
+                  <Icon name="tag" />
+                  {keyword}
+                </Label>
+              )
+            })
+          ) : (
+            <div />
+          )}
+        </p>
+      </Container>
+    ))
   }
 
   const { dataArr, name } = props
 
-  if (!dataArr || dataArr.length === 0) return <Container></Container>
+  if (!dataArr || dataArr.length === 0) return <Container />
 
   return (
     <Grid>
