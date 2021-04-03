58c511908abec3ce56d818e0272afc36780aae40
Bryan Finster <bryan.finster@gmail.com>
2021-04-03 06:29:16 -0500

    Update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,23 +13,27 @@ const About = () => (
           <img src={profileImage} width="35%" alt="Bryan Finster" />
         </div>
         <CardContent>
-          I am a developer, technical lead, product owner, value stream
-          architect, and passionate advocate for the organizational and culture
-          changes needed to optimize the flow of value to the customer. I use my
-          experience developing enterprise supply chain solutions to apply lean
-          supply chain thinking to software delivery to minimize costs and
-          maximize delivered quality.
-          <br />
-          I practice and mentor disciplined continuous delivery practices and
-          DevOps principles. I use data-driven approaches to optimize the flow
-          of value, improve business outcomes, and improve the lives of software
-          engineers.
-          <br />
-          My preferred way to see the world is from my motorcycle. I see every
-          corner as an opportunity for continuous improvement of the skills
-          needed to stay safe in an unpredictable world. Because I use my risk
-          budget for fun, I prefer to optimize my development workflow to
-          minimize risk and drama. Don&apos;t be Batman. Optimize for sleep.
+          <p>
+            I am a developer, technical lead, product owner, value stream
+            architect, and passionate advocate for the organizational and
+            culture changes needed to optimize the flow of value to the
+            customer. I use my experience developing enterprise supply chain
+            solutions to apply lean supply chain thinking to software delivery
+            to minimize costs and maximize delivered quality.
+          </p>
+          <p>
+            I practice and mentor disciplined continuous delivery practices and
+            DevOps principles. I use data-driven approaches to optimize the flow
+            of value, improve business outcomes, and improve the lives of
+            software engineers.
+          </p>
+          <p>
+            My preferred way to see the world is from my motorcycle. Every
+            corner is an opportunity for continuous improvement of the skills
+            needed to stay safe in an unpredictable world. I use my risk budget
+            for fun, I prefer to optimize my development workflow to minimize
+            risk and drama. Don&apos;t be Batman. Optimize for sleep.
+          </p>
         </CardContent>
       </Card>
     </div>
